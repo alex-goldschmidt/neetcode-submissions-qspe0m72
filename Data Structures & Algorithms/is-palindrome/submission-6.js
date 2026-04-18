a52ba3fae02f3fa48ab2228a75ccd7e8af0e1e59
Alex Goldschmidt <83550948+alex-goldschmidt@users.noreply.github.com>
2026-04-18 14:37:03 -0400

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let left = 0;
+        let right = s.length - 1;
+
+        while (left < right) {
+            while (left < right && !this.isAlphanumeric(s[left])) {
+                left++;
+            }
+
+            while (left < right && !this.isAlphanumeric(s[right])) {
+                right--;
+            }
+
+            if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    isAlphanumeric(char) {
+        return /^[a-z0-9]+$/i.test(char);
+    }
+}

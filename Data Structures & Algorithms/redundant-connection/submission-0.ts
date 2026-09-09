e06a6afe41a0a8aa9264cea23c8260e69cf2cb7e
Alex Goldschmidt <83550948+alex-goldschmidt@users.noreply.github.com>
2026-09-09 16:52:47 -0400

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.ts
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {number[][]} edges
+     * @return {number[]}
+     */
+    findRedundantConnection(edges: number[][]): number[] {
+        let visited = new Set<number>();
+        let graph = new Map<number, number[]>();
+        let res: number[][] = [];
+
+        for (let i = 0; i <= edges.length; i++) {
+            graph.set(i, []);
+        }
+
+        function dfs(node, target): boolean {
+            if (node === target) return true;
+            visited.add(node);
+            for (let neighbor of graph.get(node)) {
+                if (visited.has(neighbor)) continue;
+                if (dfs(neighbor, target)) return true;
+            }
+            return false;
+        }
+
+        for (let [node1, node2] of edges) {
+            visited.clear();
+
+            if (dfs(node1, node2)) {
+                res.push([node1, node2])
+            } else {
+                graph.get(node1).push(node2);
+                graph.get(node2).push(node1);
+            }
+        }
+
+
+
+        let last = res[res.length - 1];
+        return last;
+    }
+}

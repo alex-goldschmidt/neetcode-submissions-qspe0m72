1752437ea4aedca0dfc526b25715dcc144cbbd0e
Alex Goldschmidt <83550948+alex-goldschmidt@users.noreply.github.com>
2026-09-11 17:34:14 -0400

    Add: is-anagram - submission-15

---
+++ Data Structures & Algorithms/is-anagram/submission-15.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if (s.length !== t.length) return false;
+
+        let sDict = {};
+        let tDict = {};
+
+        for (let i = 0; i <= s.length; i++) {
+            sDict[s[i]] = (sDict[s[i]] || 0) + 1;
+            tDict[t[i]] = (tDict[t[i]] || 0) + 1;
+        }
+
+        for (let key in sDict) {
+            if (sDict[key] !== tDict[key]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}

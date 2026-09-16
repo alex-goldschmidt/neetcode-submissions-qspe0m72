b5f1fd6ac9dea8022ede0314685f7df653c04ef8
Alex Goldschmidt <83550948+alex-goldschmidt@users.noreply.github.com>
2026-09-16 16:27:34 -0400

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.ts
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles: number[], h: number): number {
+        let left = 1;
+        let right = Math.max(...piles);
+        let minEatingSpeed = right;
+
+        while (left <= right) {
+            let currentSpeed = Math.floor((left + right) / 2);
+            let totalTime = 0;
+            for (let pile of piles) {
+                let hoursToEatPile = Math.ceil(pile / currentSpeed);
+                totalTime += hoursToEatPile;
+            }
+
+            if (totalTime <= h) {
+                minEatingSpeed = currentSpeed;;
+                right = minEatingSpeed - 1;
+            } else {
+                left = currentSpeed + 1;
+            }
+        }
+
+        return minEatingSpeed;
+    }
+}
